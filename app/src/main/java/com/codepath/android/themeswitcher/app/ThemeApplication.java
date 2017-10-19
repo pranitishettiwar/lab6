45879f0c5f2066910d53aafb58b093eea3fd1d71
pranitishettiwar <praniti@lumoslabs.com>
2017-10-19 13:54:08 -0700

    Added new theme and switcher code

---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,12 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+/**
+ * Created by praniti on 10/19/17.
+ */
+
+public class ThemeApplication extends Application {
+    public static int currentPosition;
+}
+
